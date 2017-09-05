6204f9d54f4b0cbe7b31fd19606d2d460fbbd635
cab driver <cocktailsthecomic@gmail.com>
2017-09-05 03:46:46 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -15,13 +15,10 @@ function whileLoop(n){
 }
   return "done";
 }
-
-function maybeTrue() {
-  return Math.random() >= 0.5
-}
-array = [1, 2];
 function doWhileLoop(array){
-  do {array.pop}
+  function maybeTrue() {
+    return Math.random() >= 0.5}
+do {array.pop}
 while (array.length > 0 && maybeTrue());
 return array;
 }
